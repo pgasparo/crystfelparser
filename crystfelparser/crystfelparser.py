cd9c19a36729cac0ff6b6c7f4e21b80fe999aa5e
pasparo <piero.gasparotto@psi.ch>
2023-01-12 11:08:44 +0100

    Adding a more pythonic stream parser object and functions to map points in 3D

--- crystfelparser/crystfelparser.py
+++ crystfelparser/crystfelparser.py
@@ -4,12 +4,13 @@ from collections import defaultdict
 import numpy as np
 import argparse
 import sys
+import subprocess
 
 from crystfelparser.utils import save_dict_to_hdf5
 
 
 def stream_to_dictionary(streamfile):
-    """ 
+    """
     Function for parsing a indexamajig output stream
 
     Args:
@@ -28,7 +29,7 @@ def stream_to_dictionary(streamfile):
 
         return line
 
-    with open(streamfile, 'r') as text_file:
+    with open(streamfile, "r") as text_file:
         # for ln,line in enumerate(text_file):
         ln = -1
         while True:
@@ -84,10 +85,9 @@ def stream_to_dictionary(streamfile):
                     # fs/px, ss/px, (1/d)/nm^-1, Intensity
                     # with
                     # dim1 = ss, dim2 = fs
-                    tmpframe["peaks"] = np.asarray([
-                        text_file.readline().split()[:4]
-                        for tmpc in range(num_peaks)
-                    ]).astype(np.float)
+                    tmpframe["peaks"] = np.asarray(
+                        [text_file.readline().split()[:4] for tmpc in range(num_peaks)]
+                    ).astype(np.float)
 
                 ##### Get the PREDICTIONS after indexing #####
 
@@ -99,16 +99,17 @@ def stream_to_dictionary(streamfile):
                     # Get the unit cell -- as cell lengths and angles
                     line = text_file.readline().split()
                     tmpframe["Cell parameters"] = np.hstack(
-                        [line[2:5], line[6:9]]).astype(np.float)
+                        [line[2:5], line[6:9]]
+                    ).astype(np.float)
 
                     # Get the reciprocal unit cell as a 3x3 matrix
                     reciprocal_cell = []
                     for tmpc in range(3):
-                        reciprocal_cell.append(
-                            text_file.readline().split()[2:5])
+                        reciprocal_cell.append(text_file.readline().split()[2:5])
                         ln += 1
                     tmpframe["reciprocal_cell_matrix"] = np.asarray(
-                        reciprocal_cell).astype(np.float)
+                        reciprocal_cell
+                    ).astype(np.float)
 
                     # Save the lattice type
                     tmpframe["lattice_type"] = text_file.readline().split()[-1]
@@ -117,10 +118,8 @@ def stream_to_dictionary(streamfile):
                     # loop over the next 6 lines to get the diffraction resolution
                     line = loop_over_next_N_lines(text_file, 6).split()
                     ln += 6
-                    tmpframe["diffraction_resolution_limit [nm^-1]"] = np.float(
-                        line[2])
-                    tmpframe["diffraction_resolution_limit [A]"] = np.float(
-                        line[5])
+                    tmpframe["diffraction_resolution_limit [nm^-1]"] = np.float(line[2])
+                    tmpframe["diffraction_resolution_limit [A]"] = np.float(line[5])
 
                     # get the number of predicted reflections
                     num_reflections = np.int(text_file.readline().split()[-1])
@@ -140,7 +139,8 @@ def stream_to_dictionary(streamfile):
                             reflections_pos.append(line[[7, 8, 3, 4, 0, 1, 2]])
                             ln += 1
                         tmpframe["predicted_reflections"] = np.asarray(
-                            reflections_pos).astype(np.float)
+                            reflections_pos
+                        ).astype(np.float)
                     # continue reading
                     line = text_file.readline()
                     ln += 1
@@ -149,7 +149,7 @@ def stream_to_dictionary(streamfile):
                 series[im_num] = tmpframe
 
             # condition to exit the while true reading cycle
-            if ("" == line):
+            if "" == line:
                 # print("file finished")
                 break
 
@@ -158,12 +158,14 @@ def stream_to_dictionary(streamfile):
 
 
 def dictionary_parsed_to_h5(parsed_stream, outputfile):
-    """ Save a nested dictionary to h5. """
-    idx_frames = np.asarray([fr for fr in parsed_stream.keys()])[np.where(
-        np.asarray([len(fr.keys()) for fr in parsed_stream.values()]) == 13)[0]]
+    """Save a nested dictionary to h5."""
+    idx_frames = np.asarray([fr for fr in parsed_stream.keys()])[
+        np.where(np.asarray([len(fr.keys()) for fr in parsed_stream.values()]) == 13)[0]
+    ]
 
     indexed_frames = dict({fr: parsed_stream[fr] for fr in idx_frames})
     save_dict_to_hdf5(indexed_frames, outputfile)
+
 
 ###################################################
 # Script to be called from the command line
@@ -171,17 +173,18 @@ def dictionary_parsed_to_h5(parsed_stream, outputfile):
 
 def parse_args():
     """Parser"""
-    parser = argparse.ArgumentParser(
-        description='Console script to parse indexamajig.')
+    parser = argparse.ArgumentParser(description="Console script to parse indexamajig.")
 
     parser.add_argument(
-        '--stream', default="input.stream", help='Streaming file to parse [e.g. input.stream]'
+        "--stream",
+        default="input.stream",
+        help="Streaming file to parse [e.g. input.stream]",
     )
 
     parser.add_argument(
-        '--output',
+        "--output",
         default="parsed_stream.h5",
-        help='Parsed file, stored in hdf5 format -- only indexable frames are stored! [default: parsed_stream.h5]',
+        help="Parsed file, stored in hdf5 format -- only indexable frames are stored! [default: parsed_stream.h5]",
     )
 
     if len(sys.argv) == 1:
@@ -191,6 +194,257 @@ def parse_args():
         # args.func(**vars(args))
 
         return args
+
+
+class streamfile_parser:
+    def __init__(self, streamfile):
+        self.streamfile = streamfile
+        self.parsed = self.stream_to_dictionary()
+        (
+            self.beam_center_x,
+            self.beam_center_y,
+            self.clen,
+            self.wavelength,
+            self.cellpdb,
+        ) = self.get_experiment_info()
+        self.cell_matrix = np.array(
+            [[self.cellpdb[0], 0, 0], [0, self.cellpdb[1], 0], [0, 0, self.cellpdb[2]]]
+        )
+        # self.reciprocal_cell_matrix = np.linalg.inv(self.cell_matrix)
+        self.reciprocal_cell_matrix = np.linalg.inv(self.cell_matrix)
+
+    def stream_to_dictionary(self):
+        stream_to_dictionary(self.streamfile)
+
+    def get_experiment_info(self):
+        """
+        get same info about the experiment from the stream file
+        """
+        proc = subprocess.Popen(
+            "grep corner_x {}".format(self.streamfile),
+            stdout=subprocess.PIPE,
+            shell=True,
+        )
+        (out, err) = proc.communicate()
+        out = out.decode("UTF-8")
+        posx = float(out.split()[2])
+
+        proc = subprocess.Popen(
+            "grep corner_y {}".format(self.streamfile),
+            stdout=subprocess.PIPE,
+            shell=True,
+        )
+        (out, err) = proc.communicate()
+        out = out.decode("UTF-8")
+        posy = float(out.split()[2])
+        proc = subprocess.Popen(
+            "grep clen {}".format(self.streamfile), stdout=subprocess.PIPE, shell=True
+        )
+        (out, err) = proc.communicate()
+        out = out.decode("UTF-8")
+        clen = float(out.split()[2]) * 1000
+
+        proc = subprocess.Popen(
+            "grep 'photon_energy' {}".format(self.streamfile),
+            stdout=subprocess.PIPE,
+            shell=True,
+        )
+        (out, err) = proc.communicate()
+        out = out.decode("UTF-8")
+        photon_energy = 12398.42 / np.float(
+            max([int(i) for i in set(out.split()) if i.isnumeric()])
+        )
+
+        proc = subprocess.Popen(
+            "grep -A11 'Begin unit cell'  {}".format(self.streamfile),
+            stdout=subprocess.PIPE,
+            shell=True,
+        )
+        (out, err) = proc.communicate()
+        out = out.decode("UTF-8")
+        cellstr = [
+            line
+            for line in out.split("\n")
+            if len(line) > 0
+            if len(line) > 1
+            if line.split()[0] in {"a", "b", "c", "al", "be", "ga"}
+        ]
+        cell = np.array([np.float(line.split()[2]) for line in cellstr])
+        return abs(posx), abs(posy), clen, photon_energy, cell
+
+    def get_indexable_frames(self):
+        """
+        Returns a list of indexable frames.
+        """
+        return np.array(
+            [frame for frame, info in self.parsed.items() if len(info.keys()) > 7]
+        )
+
+    def get_cellslist(self):
+        """
+        Returns a list of reciprocal cells matrices
+        """
+        return np.array(
+            [
+                np.array(
+                    np.linalg.inv(self.parsed[frame]["reciprocal_cell_matrix"] / 10.0)
+                )
+                for frame in self.get_indexable_frames()
+            ]
+        )
+
+    def get_spots_2d(self, frame):
+        """
+        Returns a the spots in the detector panel (px units)
+        relative to the spcified the input frame
+        """
+        if "peaks" in self.parsed[frame].keys():
+            return self.parsed[frame]["peaks"][:, :2]
+        else:
+            return np.array([])
+
+    def rodrigues(self, h, phi, rot_ax):
+
+        cp = np.cos(phi)
+        sp = np.sin(phi)
+        omcp = 1.0 - cp
+
+        rot_h = np.zeros(3)
+
+        rot_h[0] = (
+            (cp + rot_ax[0] ** 2 * omcp) * h[0]
+            + (-rot_ax[2] * sp + rot_ax[0] * rot_ax[1] * omcp) * h[1]
+            + (rot_ax[1] * sp + rot_ax[0] * rot_ax[2] * omcp) * h[2]
+        )
+        rot_h[1] = (
+            (rot_ax[2] * sp + rot_ax[0] * rot_ax[1] * omcp) * h[0]
+            + (cp + rot_ax[1] ** 2 * omcp) * h[1]
+            + (-rot_ax[0] * sp + rot_ax[1] * rot_ax[2] * omcp) * h[2]
+        )
+        rot_h[2] = (
+            (-rot_ax[1] * sp + rot_ax[0] * rot_ax[2] * omcp) * h[0]
+            + (rot_ax[1] * sp + rot_ax[1] * rot_ax[2] * omcp) * h[1]
+            + (cp + rot_ax[2] ** 2 * omcp) * h[2]
+        )
+
+        return rot_h
+
+    def get_spots_3d(self, frame):
+        """
+        Maps the spots in 2D into 3D points in the reciprocal space.
+        """
+
+        wavelength = self.wavelength
+        # size of the pixels
+        qx = 0.075000
+        qy = 0.075000
+        orgx = self.beam_center_x
+        orgy = self.beam_center_y
+        det_dist = self.clen
+        det_x = np.asarray([1.0, 0.0, 0.0])
+        det_y = np.asarray([0.0, 1.0, 0.0])
+        resolmax = 0.1
+        resolmin = 999
+        starting_angle = 0
+        oscillation_range = 0.0
+        rot_ax = np.asarray([1.000000, 0.0, 0.0])
+
+        # get the reference frame
+        incident_beam = np.asarray([0.0, 0.0, 1.0 / wavelength])
+        det_z = np.zeros(3)
+        # comput z in case x,y are not perpendicular to the beam
+        det_z[0] = (
+            det_x[1] * det_y[2] - det_x[2] * det_y[1]
+        )  # calculate detector normal -
+        det_z[1] = det_x[2] * det_y[0] - det_x[0] * det_y[2]  # XDS.INP does not have
+        det_z[2] = det_x[0] * det_y[1] - det_x[1] * det_y[0]  # this item.
+        det_z = det_z / np.sqrt(np.dot(det_z, det_z))  # normalize (usually not req'd)
+
+        spots = []
+
+        for line in self.get_spots_2d(frame):
+            (ih, ik, il) = (0.0, 0.0, 0.0)
+            if len(line) == 4:
+                (x, y, phi, intensity) = line
+            elif len(line) == 7:
+                (x, y, phi, intensity, ih, ik, il) = line
+            elif len(line) == 3:
+                (x, y, intensity) = line
+                phi = 0.0
+            else:
+                (x, y) = line
+                phi = 0.0
+                intensity = 0.0
+
+            # convert detector coordinates to local coordinate system
+            r = np.asarray(
+                [
+                    (x - orgx) * qx * det_x[0]
+                    + (y - orgy) * qy * det_y[0]
+                    + det_dist * det_z[0],
+                    (x - orgx) * qx * det_x[1]
+                    + (y - orgy) * qy * det_y[1]
+                    + det_dist * det_z[1],
+                    (x - orgx) * qx * det_x[2]
+                    + (y - orgy) * qy * det_y[2]
+                    + det_dist * det_z[2],
+                ]
+            )
+
+            # normalize scattered vector to obtain S1
+            r = r / (wavelength * np.sqrt(np.dot(r, r)))
+            # obtain reciprocal space vector S = S1-S0
+            r = r - incident_beam
+
+            if np.sqrt(np.dot(r, r)) > 1.0 / resolmax:
+                continue  # outer resolution limit
+            if np.sqrt(np.dot(r, r)) < 1.0 / resolmin:
+                continue  # inner resolution limit
+
+            # rotate
+            # NB: the term "-180." (found by trial&error) seems to make it match dials.rs_mapper
+            phi = (starting_angle + oscillation_range * phi - 180.0) / 180.0 * np.pi
+            rot_r = self.rodrigues(r, phi, rot_ax)
+
+            # rot_r=100.*rot_r + 100./resolmax  # ! transform to match dials.rs_mapper
+
+            spots.append(np.hstack([rot_r, [intensity], [ih, ik, il]]))
+
+        return np.asarray(spots)[:, :3]
+
+    def indexed2file(self, h5file, outputname):
+        """
+        Write to a text file the indexing solutions in a format readable by indexamajig
+        """
+        with open(outputname, "w") as text_file:
+            for fr, tmpcell in zip(self.get_indexable_frames(), self.get_cellslist()):
+                reccel = np.array(self.get_reciprocal_basis(tmpcell)) * 10
+                line = "{} //{} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} {:.8f} 0 0 aP\n".format(
+                    h5file, fr, *np.array(reccel).ravel()
+                )
+                text_file.write(line)
+
+    def get_reciprocal_basis(self, primal_basis, with_correction=False):
+        """Finds the reciprocal basis for a given primal basis in direct space.
+
+        Args:
+            primal_basis: a numpy array with shape (3,3) representing the primal basis in the direct space.
+            with_correction: a boolean flag indicating whether to apply a correction to the reciprocal basis. Default: False
+        Returns:
+            A numpy array with shape (3,3) representing the reciprocal basis.
+        """
+        a1 = primal_basis[:, 0]
+        a2 = primal_basis[:, 1]
+        a3 = primal_basis[:, 2]
+        b1 = np.cross(a2, a3) / (np.dot(a1, np.cross(a2, a3)))
+        b2 = np.cross(a3, a1) / (np.dot(a1, np.cross(a2, a3)))
+        b3 = np.cross(a1, a2) / (np.dot(a1, np.cross(a2, a3)))
+        basis = np.column_stack([b1, b2, b3])
+        if with_correction:
+            mask = np.array([[-1.0, 1.0, 1.0], [1.0, -1.0, -1.0], [-1.0, 1.0, 1.0]])
+            return (basis * mask).T
+        else:
+            return basis.T
 
 
 def main():
